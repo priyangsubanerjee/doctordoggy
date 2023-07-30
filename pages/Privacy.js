d0365615f3203bbb5668ef6e4395e8f923d25448
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-07-30 22:04:07 +0530

    Privacy updated

---
+++ pages/Privacy.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Privacy() {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo accusamus
+        unde inventore dicta possimus, rem, perferendis, quod a ex ullam
+        perspiciatis excepturi laboriosam neque? Praesentium veniam voluptatem
+        ab eveniet aliquam.
+      </p>
+    </div>
+  );
+}
+
+export default Privacy;
