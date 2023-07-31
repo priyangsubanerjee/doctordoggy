6a47681568d55fe40ad5cfef814d3d128b72d7d4
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-07-31 17:30:16 +0530

    edit

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -30,13 +30,6 @@ function Profile() {
           <h2 className="text-lg lg:text-2xl font-semibold text-neutral-800">
             Your <span className="text-pink-500">account</span>
           </h2>
-          <button className="flex items-center text-xs space-x-2 font-bold tracking-wider text-pink-600">
-            <iconify-icon
-              height="16"
-              icon="fluent:edit-20-regular"
-            ></iconify-icon>
-            <span>EDIT</span>
-          </button>
         </div>
         <p className="text-[11px] lg:text-xs text-neutral-500 mt-1">
           Manage your account preferences.
@@ -116,6 +109,40 @@ function Profile() {
             <p>priyangs26@gmail.com</p>
           </div>
         </div>
+        <div className="space-y-2">
+          <label
+            htmlFor=""
+            className="flex items-center space-x-2 text-neutral-700"
+          >
+            <span className="text-xs">Pincode</span>
+          </label>
+          <div className="border px-3 space-x-3 py-3 bg-neutral-50 flex items-center text-sm rounded-md">
+            <span className="text-neutral-500">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M19.183 7.805L16.22 4.838c-2.027-2.03-3.04-3.043-4.129-2.803c-1.088.24-1.581 1.587-2.568 4.28l-.668 1.823c-.263.718-.395 1.077-.632 1.355a2.035 2.035 0 0 1-.36.332c-.296.213-.664.314-1.4.517c-1.66.458-2.491.687-2.804 1.23a1.528 1.528 0 0 0-.204.773c.004.627.613 1.236 1.83 2.455L6.7 16.216l-4.476 4.48a.764.764 0 0 0 1.08 1.08l4.475-4.48l1.466 1.468c1.226 1.226 1.839 1.84 2.47 1.84c.265 0 .526-.068.757-.2c.548-.313.778-1.149 1.239-2.822c.202-.735.303-1.102.515-1.399c.093-.129.201-.247.322-.352c.275-.238.632-.372 1.345-.64l1.844-.693c2.664-1 3.996-1.501 4.23-2.586c.235-1.086-.77-2.093-2.783-4.107Z"
+                />
+              </svg>
+            </span>
+            <p>713216</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex items-center justify-end mt-8 max-w-sm">
+        <button className="flex items-center text-sm space-x-2 text-white bg-neutral-800 px-5 py-3 rounded-md">
+          <iconify-icon
+            height="16"
+            icon="fluent:edit-20-regular"
+          ></iconify-icon>
+          <span>Edit account</span>
+        </button>
       </div>
     </div>
   );
