db78929895e6d6879270d0153287e8e0617c019d
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-08-02 13:38:57 +0530

    async

--- middleware.js
+++ middleware.js
@@ -1,11 +1,15 @@
 import { NextResponse } from "next/server";
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request) {
-  return NextResponse.redirect(new URL("/profile", request.url));
+export async function middleware(request) {
+  if (request.nextUrl.pathname == "/dashboard") {
+    if (request.cookies.get("user")) {
+      let decodeUrl = new URL("/api/user/decode", request.url);
+    }
+  }
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: "/",
+  matcher: ["/dashboard"],
 };
