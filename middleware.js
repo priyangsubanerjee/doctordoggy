db1706cc37dfef1d3486852ac0a77ac7c88749be
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-08-02 13:41:57 +0530

    middleware

--- middleware.js
+++ middleware.js
@@ -5,6 +5,21 @@ export async function middleware(request) {
   if (request.nextUrl.pathname == "/dashboard") {
     if (request.cookies.get("user")) {
       let decodeUrl = new URL("/api/user/decode", request.url);
+      let res = await fetch(decodeUrl, {
+        method: "POST",
+        body: JSON.stringify({
+          token: request.cookies.get("user").value,
+        }),
+      });
+      let data = await res.json();
+      console.log(data.user);
+      if (data.status == "success") {
+        return NextResponse.next();
+      }
+    } else {
+      return NextResponse.redirect(
+        new URL("/profile?setup=true&focus=phone", request.url)
+      );
     }
   }
 }
