04aa9472391d7cc6777902b990ea37dad99c8d56
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-08-04 01:34:02 +0530

    upload

--- pages/api/pets/register.js
+++ pages/api/pets/register.js
@@ -20,10 +20,10 @@ export default async function handler(req, res) {
       name,
       family,
       sex,
-      dateOfBirth,
+      dateOfBirth: dateOfBirth.toString(),
       breed,
       color,
-      weight,
+      weight: weight.toString(),
       complications,
       parentEmail,
     });
