37aa67d8b80c5d0c104531919176e4f551f30a24
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-08-10 21:16:06 +0530

    v-record

--- components/Pets/VaccineCard.js
+++ components/Pets/VaccineCard.js
@@ -18,7 +18,11 @@ function VaccineCard({ record, pet }) {
                 weekday: "long",
               })
               .toUpperCase()
-          : record.dueDate +
+          : record.dueDate.split("-")[2] +
+            "-" +
+            record.dueDate.split("-")[1] +
+            "-" +
+            record.dueDate.split("-")[0] +
             " | " +
             new Date(record.dueDate)
               .toLocaleDateString("en-US", {
@@ -37,7 +41,7 @@ function VaccineCard({ record, pet }) {
         ) : (
           <Link href={`/pets/${pet._id}/vaccination/${record._id}`}>
             <button className="px-4 py-2 font-medium text-sm bg-blue-50 text-blue-900 rounded-md mr-5">
-              Review certificate
+              View certificate
             </button>
           </Link>
         )}
--- pages/pets/[id]/vaccination/[vid]/index.js
+++ pages/pets/[id]/vaccination/[vid]/index.js
@@ -39,13 +39,124 @@ function Vaccination({ pet, vaccination }) {
   console.log(vaccination);
   return (
     <div className="min-h-screen px-6 py-8 lg:py-16 lg:px-[100px]">
-      <div className="flex space-x-2 items-center lg:max-w-xl">
-        <span className="text-lg lg:text-2xl font-semibold text-neutral-800">
-          <iconify-icon height="50" icon="ph:prescription-light"></iconify-icon>{" "}
-        </span>
-        <span>
-          for <span className="font-semibold">{pet.name}</span>
-        </span>
+      <div className="flex items-center">
+        <div className="flex items-center space-x-2 py-2 px-3 bg-green-50 text-green-700 font-medium text-sm rounded-lg">
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M21 11c0 5.55-3.84 10.74-9 12c-5.16-1.26-9-6.45-9-12V5l9-4l9 4v6m-9 10c3.75-1 7-5.46 7-9.78V6.3l-7-3.12L5 6.3v4.92C5 15.54 8.25 20 12 21Z"
+              />
+            </svg>
+          </span>
+          <span>Vaccination done</span>
+        </div>
+      </div>
+      <div className="mt-10">
+        <h2 className="text-4xl text-neutral-700 font-bold">
+          {vaccination.vaccineName}
+        </h2>
+        <div className="mt-4">
+          {vaccination.doctor?.id == null && (
+            <div className="text-sm flex items-center">
+              <span className="inline-block">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                >
+                  <g
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="1.5"
+                  >
+                    <circle cx="8" cy="8" r="6.25" />
+                    <path d="M8 5.25v0m0 6v-3.5" />
+                  </g>
+                </svg>
+              </span>
+              <p className="ml-1 inline-block">
+                We are sorry, but we could not find the doctor who administered
+                this vaccine. Thus we do not claim any responsibility for the
+                data.
+              </p>
+            </div>
+          )}
+        </div>
+        <div className="grid grid-cols-3 lg:grid-cols-4 gap-7 lg:gap-7 mt-10 lg:max-w-4xl">
+          <div className="col-span-3 lg:col-span-1 ">
+            <span className="tracking-wide text-xs text-neutral-500">
+              Vaccine for
+            </span>
+            <p className="mt-2">{pet.name}</p>
+          </div>
+          <div>
+            <span className="tracking-wide text-xs text-neutral-500">
+              Species
+            </span>
+            <p className="mt-2">{pet.family}</p>
+          </div>
+          <div>
+            <span className="tracking-wide text-xs text-neutral-500">
+              Colour
+            </span>
+            <p className="mt-2">{pet.color}</p>
+          </div>
+          <div>
+            <span className="tracking-wide text-xs text-neutral-500">
+              Date of birth
+            </span>
+            <p className="mt-2">{pet.dateOfBirth}</p>
+          </div>
+          <div className="col-span-2 lg:col-span-1">
+            <span className="tracking-wide text-xs text-neutral-500">
+              Breed
+            </span>
+            <p className="mt-2">{pet.breed}</p>
+          </div>
+          <div className="col-span-1 lg:col-span-1">
+            <span className="tracking-wide text-xs text-neutral-500">Sex</span>
+            <p className="mt-2">{pet.sex}</p>
+          </div>
+          <div className="col-span-3 lg:col-span-2">
+            <span className="tracking-wide text-xs text-neutral-500">
+              Complication history
+            </span>
+            <p className="mt-2">{pet.historyOfComplications || "--"}</p>
+          </div>
+          <div className="">
+            <span className="tracking-wide text-xs text-neutral-500">
+              Due date
+            </span>
+            <p className="mt-2">{vaccination.dueDate || "--"}</p>
+          </div>
+          <div className="">
+            <span className="tracking-wide text-xs text-neutral-500">
+              Vaccinated on
+            </span>
+            <p className="mt-2">{vaccination.dueDate || "--"}</p>
+          </div>
+          <div className="">
+            <span className="tracking-wide text-xs text-neutral-500">
+              Vaccinated by
+            </span>
+            <p className="mt-2">{vaccination.doctor?.name || "--"}</p>
+          </div>
+          <div className="lg:col-span-3">
+            <span className="tracking-wide text-xs text-neutral-500">
+              Additional notes
+            </span>
+            <p className="mt-2">{vaccination.notes || "--"}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
