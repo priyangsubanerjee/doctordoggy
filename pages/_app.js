f053b1ae918062e73cce56703f68ae1715d9ff89
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-08-26 11:47:45 +0530

    loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable react-hooks/exhaustive-deps */
 import Footer from "@/components/Footer";
 import Layout from "@/components/Layout";
 import Navbar from "@/components/Navbar";
 import GlobalStates from "@/context/GlobalState";
 import "@/styles/globals.css";
+import { Icon } from "@iconify/react";
+import { set } from "mongoose";
 
 import { SessionProvider } from "next-auth/react";
 import NextProgress from "next-progress";
@@ -16,6 +19,24 @@ export default function App({
 }) {
   const [pets, setPets] = useState(null);
   const [account, setAccount] = useState(null);
+  const [loadingOn, setLoadingOn] = useState(true);
+
+  useEffect(() => {
+    let shown = sessionStorage.getItem("loadingShown") || false;
+    let loadingScreen = document.getElementById("loadingScreen");
+    if (shown) {
+      setLoadingOn(false);
+      return;
+    } else {
+      loadingScreen.style.transform = "translateX(0%)";
+      setTimeout(() => {
+        loadingScreen.style.transform = "translateX(-100%)";
+        sessionStorage.setItem("loadingShown", true);
+        setLoadingOn(false);
+      }, 3000);
+    }
+  }, []);
+
   const refreshPets = async () => {
     let email = session?.user?.email || null;
     if (email) {
@@ -73,25 +94,42 @@ export default function App({
     >
       <SessionProvider session={session}>
         <>
-          <NextProgress height={"3px"} options={{ showSpinner: false }} />
-          <Navbar />
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-          <Footer />
-          <Toaster
-            position="bottom-right"
-            containerStyle={{
-              bottom: 50,
-              right: 50,
-            }}
-            toastOptions={{
-              style: {
-                background: "#333",
-                color: "#fff",
-              },
-            }}
-          />
+          <div
+            id="loadingScreen"
+            className="fixed transition-all duration-700 -translate-x-full inset-0 h-full w-full z-40 bg-[#F15958] flex flex-col items-center justify-center"
+          >
+            <img
+              src="https://res.cloudinary.com/ddn3h4a2b/image/upload/v1692890038/static/IMG_1494_o1olve.png"
+              className="max-w-[300px] mix-blend-multiply"
+              alt=""
+            />
+            <span className="text-white mt-20">
+              <Icon height={40} icon="line-md:loading-twotone-loop" />
+            </span>
+          </div>
+          {loadingOn == false && (
+            <>
+              <NextProgress height={"3px"} options={{ showSpinner: false }} />
+              <Navbar />
+              <Layout>
+                <Component {...pageProps} />
+              </Layout>
+              <Footer />
+              <Toaster
+                position="bottom-right"
+                containerStyle={{
+                  bottom: 50,
+                  right: 50,
+                }}
+                toastOptions={{
+                  style: {
+                    background: "#333",
+                    color: "#fff",
+                  },
+                }}
+              />
+            </>
+          )}
         </>
       </SessionProvider>
     </GlobalStates.Provider>
