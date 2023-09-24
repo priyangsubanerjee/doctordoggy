aa6e30a9dc74ee0e405e05b33124aadfccf5c312
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-09-24 22:00:56 +0530

    success

--- pages/bookings/success.js
+++ pages/bookings/success.js
@@ -1,8 +1,31 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 
 function Success() {
   return (
-    <div className="min-h-screen px-6 py-8 lg:py-16 lg:px-[100px]">Success</div>
+    <div className="min-h-screen px-6 py-8 lg:py-16 lg:px-[100px]">
+      <div className="flex flex-col items-center justify-center">
+        <img
+          src="https://png.pngtree.com/png-vector/20221113/ourmid/pngtree-happy-dog-cartoon-character-png-image_6439369.png"
+          alt=""
+          className="h-44"
+        />
+        <h2 className="text-2xl font-bold text-slate-800 mt-7">
+          Your booking has been successfully created.
+        </h2>
+        <h3 className="text-sm mt-3">
+          We will contact you shortly on your registered number.
+        </h3>
+        <div className="flex items-center space-x-4 text-sm mt-10">
+          <button className="h-12 px-4 bg-neutral-800 text-white rounded">
+            View bookings
+          </button>
+          <button className="h-12 px-4 bg-neutral-200 text-black rounded">
+            Go to dashboard
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
 
