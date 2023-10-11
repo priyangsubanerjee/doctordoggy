981871ee9f73813ae5f2dbc539ae3c35021058f5
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-10-11 20:00:01 +0530

    parents

--- pages/account/index.js
+++ pages/account/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Account() {
-  return <div>Account</div>;
+  return (
+    <div className="lg:px-44 py-16">
+      <h1 className="text-3xl font-medium">Account</h1>
+      <div className="flex items-center space-x-2">
+        <span>Unable to access this page?</span>
+      </div>
+    </div>
+  );
 }
 
 export default Account;
