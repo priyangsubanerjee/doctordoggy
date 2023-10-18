5fa0df0ee7e70846ec3072ebee39aaa9b633870d
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-10-19 01:59:53 +0530

    logo updated

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,24 @@ import HeroSection from "@/components/Fragments/HeroSection";
 import Services from "@/components/Fragments/Services";
 import StepsToEnjoy from "@/components/Fragments/StepsToEnjoy";
 import Founders from "@/components/Fragments/Founders";
+import { useEffect } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  useEffect(() => {
+    // ask for notification permission on page load
+    Notification.requestPermission().then(function (result) {
+      console.log(result);
+    });
+
+    // if granted then send notification to user
+
+    // if (Notification.permission === "granted") {
+    //   navigator.serviceWorker.getRegistration().then(function (reg) {
+    //     reg.showNotification("Sheduled vaccination on 10th");
+    //   });
+    // }
+  }, []);
   return (
     <main>
       <HeroSection />
