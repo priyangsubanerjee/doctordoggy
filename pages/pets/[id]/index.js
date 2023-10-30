71d5c0059fa7c0784b3eca4d4a070f8896b59043
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-10-30 23:42:51 +0530

    tabs

--- pages/pets/[id]/index.js
+++ pages/pets/[id]/index.js
@@ -33,7 +33,61 @@ function PetDashboard({ pet }) {
     "Pathology",
   ];
 
+  const [tabChooserOpen, setTabChooserOpen] = useState(false);
   const [selectedTab, setSelectedTab] = useState(tabOptions[0]);
+
+  const TabChooser = ({}) => {
+    if (tabChooserOpen) {
+      return (
+        <div className="fixed inset-0 z-20 bg-black/50 h-full w-full flex items-end">
+          <div className="pb-6 bg-white w-full">
+            <div className="p-7">
+              <div className="flex items-center justify-between">
+                <h1 className="text-lg font-semibold text-slate-700">
+                  Choose destination
+                </h1>
+                <button
+                  onClick={() => setTabChooserOpen(false)}
+                  className="h-7 w-7 rounded-full bg-neutral-100 flex items-center justify-center"
+                >
+                  <Icon icon="ic:round-close" />
+                </button>
+              </div>
+              <p></p>
+
+              <ul className="mt-6">
+                {tabOptions.map((tab, index) => {
+                  return (
+                    <li key={index} className="mt-3">
+                      <button
+                        onClick={() => {
+                          setSelectedTab(tab);
+                          setTabChooserOpen(false);
+                        }}
+                        key={index}
+                        className={`flex items-center justify-center h-full relative py-1 text-sm ${
+                          selectedTab == tab
+                            ? "text-slate-800"
+                            : "text-slate-800/40"
+                        }`}
+                      >
+                        {tab}
+                        <div
+                          className={`absolute inset-x-0 -bottom-[1px] h-[1px] ${
+                            selectedTab == tab ? "bg-black" : "bg-black/0"
+                          }`}
+                        ></div>
+                      </button>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  };
 
   const Tabs = ({}) => {
     return (
@@ -59,13 +113,38 @@ function PetDashboard({ pet }) {
           })}
         </div>
         <div className="flex lg:hidden items-center justify-between px-10 mt-10">
-          <button className="text-neutral-500">
+          <button
+            onClick={() => {
+              let index = tabOptions.indexOf(selectedTab);
+              if (index == 0) {
+                index = tabOptions.length - 1;
+              } else {
+                index--;
+              }
+              setSelectedTab(tabOptions[index]);
+            }}
+            className="text-neutral-500"
+          >
             <Icon height={20} icon="uiw:left" />
           </button>
-          <p className="text-sm w-fit bg-neutral-100 px-5 py-1 rounded-full text-neutral-700 cursor-pointer">
+          <p
+            onClick={() => setTabChooserOpen(true)}
+            className="text-sm w-fit bg-neutral-100 px-5 py-1 rounded-full text-neutral-700 cursor-pointer"
+          >
             {selectedTab}
           </p>
-          <button className="text-neutral-500">
+          <button
+            onClick={() => {
+              let index = tabOptions.indexOf(selectedTab);
+              if (index == tabOptions.length - 1) {
+                index = 0;
+              } else {
+                index++;
+              }
+              setSelectedTab(tabOptions[index]);
+            }}
+            className="text-neutral-500"
+          >
             <Icon height={20} icon="uiw:right" />
           </button>
         </div>
@@ -95,9 +174,10 @@ function PetDashboard({ pet }) {
           </span>
         </h1>
         <p className="text-center mt-2 text-sm text-neutral-700">
-          Goodest boy in the town !
+          Goodest {pet.sex == "male" ? "boy" : "girl"} in the town !
         </p>
         <Tabs />
+        <TabChooser />
       </div>
     );
   } else {
