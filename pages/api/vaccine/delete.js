a9a2e69365edf65d62791aaccfc8ae17e0bfb917
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-11-01 19:22:58 +0530

    deleteAPI

--- pages/api/vaccine/delete.js
+++ pages/api/vaccine/delete.js
@@ -1,14 +1,14 @@
-// import { deleteVaccineById } from "@/prisma/vaccine";
+import { deleteVaccineById } from "@/prisma/vaccine";
 
-// export default async function handler(req, res) {
-//   const { id } = req.body;
-//   try {
-//     const vaccine = await deleteVaccineById(id);
-//     res.status(200).json({
-//       message: "Vaccine deleted successfully",
-//       vaccine: vaccine,
-//     });
-//   } catch (error) {
-//     res.status(200).json({ message: "Something went wrong" });
-//   }
-// }
+export default async function handler(req, res) {
+  const { id } = req.body;
+  try {
+    const vaccine = await deleteVaccineById(id);
+    res.status(200).json({
+      message: "Vaccine deleted successfully",
+      vaccine: vaccine,
+    });
+  } catch (error) {
+    res.status(200).json({ message: "Something went wrong" });
+  }
+}
