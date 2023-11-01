555687687856f5a021e677a4cccb84390647f761
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-11-01 19:03:06 +0530

    re trey

--- pages/api/vaccine/create.js
+++ pages/api/vaccine/create.js
@@ -1,14 +1,14 @@
-import { scheduleVaccine } from "@/prisma/vaccine";
+// import { scheduleVaccine } from "@/prisma/vaccine";
 
-export default async function handler(req, res) {
-  const vaccineProp = req.body;
-  try {
-    let vaccineCreated = await scheduleVaccine(vaccineProp);
-    res.status(200).json({
-      message: "Vaccine scheduled successfully",
-      vaccine: vaccineCreated,
-    });
-  } catch (error) {
-    res.status(200).json({ message: "Something went wrong" });
-  }
-}
+// export default async function handler(req, res) {
+//   const vaccineProp = req.body;
+//   try {
+//     let vaccineCreated = await scheduleVaccine(vaccineProp);
+//     res.status(200).json({
+//       message: "Vaccine scheduled successfully",
+//       vaccine: vaccineCreated,
+//     });
+//   } catch (error) {
+//     res.status(200).json({ message: "Something went wrong" });
+//   }
+// }
