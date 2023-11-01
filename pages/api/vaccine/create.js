ed0d7a337dc6de7451016064d081e1d07df1f5ed
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-11-01 17:25:52 +0530

    error

--- pages/api/vaccine/create.js
+++ pages/api/vaccine/create.js
@@ -8,6 +8,7 @@ export default async function handler(req, res) {
       message: "Vaccine scheduled successfully",
       vaccine: vaccineCreated,
     });
-  } catch (error) {}
-  res.status(200).json({ message: "Something went wrong" });
+  } catch (error) {
+    res.status(200).json({ message: "Something went wrong" });
+  }
 }
