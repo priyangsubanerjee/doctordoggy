979e1312cc00ca991ce1e9604f82ba1abb419319
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-11-28 11:25:41 +0530

    service

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,12 +1,18 @@
-import firebaseConfig from "./firebaseConfig";
-
 // eslint-disable-next-line no-undef
 importScripts("https://www.gstatic.com/firebasejs/8.8.0/firebase-app.js");
 // eslint-disable-next-line no-undef
 importScripts("https://www.gstatic.com/firebasejs/8.8.0/firebase-messaging.js");
 
 // eslint-disable-next-line no-undef
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp({
+  apiKey: "AIzaSyBl5wGQfcCgdGkHa5hH9BweWTf3TU4XJRk",
+  authDomain: "massive-boulder-394311.firebaseapp.com",
+  projectId: "massive-boulder-394311",
+  storageBucket: "massive-boulder-394311.appspot.com",
+  messagingSenderId: "61426030462",
+  appId: "1:61426030462:web:000c92171385846bf7452f",
+  measurementId: "G-085VV3TNDS",
+});
 // eslint-disable-next-line no-undef
 const messaging = firebase.messaging();
 
