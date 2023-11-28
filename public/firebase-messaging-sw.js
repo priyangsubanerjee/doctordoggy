4c7ce4a1cb605bfd2315b2c10f532c9c20fc65f0
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-11-28 12:12:25 +0530

    logo

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -24,7 +24,7 @@ messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: "./logo.png",
+    icon: "./logoDark.png",
   };
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
