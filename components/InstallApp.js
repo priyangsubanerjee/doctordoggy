2c026dad9fd51c043e0115248ee051cedad844d8
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-11-28 21:15:57 +0530

    install app modal

--- components/InstallApp.js
+++ components/InstallApp.js
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import GlobalStates from "@/context/GlobalState";
 import { Icon } from "@iconify/react";
+import { AnimatePresence, motion } from "framer-motion";
 import React, { useContext, useEffect, useState } from "react";
 
 function InstallApp() {
@@ -93,45 +94,53 @@ function InstallApp() {
 
   return (
     <>
-      {isVisible && (
-        <div className="h-full flex items-end lg:items-center justify-center w-full z-40 bg-black/70 inset-0 fixed">
-          <div className="h-auto w-full px-3 pb-4">
-            <div className="w-full h-full relative bg-neutral-100/80 backdrop-blur-lg rounded-3xl py-6">
-              <div className="flex items-center px-6">
-                <div className="h-14 w-14 flex items-center justify-center bg-white rounded-lg p-2">
-                  <img src="/logoDark.png" alt="" />
+      <AnimatePresence>
+        {isVisible && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2 }}
+            className="h-full flex items-end lg:items-center justify-center w-full z-40 bg-black/70 inset-0 fixed"
+          >
+            <div className="h-auto w-full px-3 pb-4">
+              <div className="w-full h-full relative bg-neutral-100/80 backdrop-blur-lg rounded-3xl py-6">
+                <div className="flex items-center px-6">
+                  <div className="h-14 w-14 flex items-center justify-center bg-white rounded-lg p-2">
+                    <img src="/logoDark.png" alt="" />
+                  </div>
+                  <div className="ml-3">
+                    <h1 className="font-semibold">Install Doctor Doggy</h1>
+                    <p className="text-xs text-neutral-500 mt-1">
+                      Never miss an important date
+                    </p>
+                  </div>
                 </div>
-                <div className="ml-3">
-                  <h1 className="font-semibold">Install Doctor Doggy</h1>
-                  <p className="text-xs text-neutral-500 mt-1">
-                    Never miss an important date
+                <div className="h-[1px] w-auto bg-neutral-800/20 mt-6"></div>
+                <div className="p-5">
+                  <p className="text-sm leading-6">
+                    To receive push notifications in your iOS device you need to
+                    add this to home screen. Follow the steps below.
+                  </p>
+
+                  <p className="mt-10 text-sm flex items-center">
+                    1. Tap on{" "}
+                    <span className="bg-white p-2 text-blue-600 ml-1 rounded-md inline-flex items-center justify-center">
+                      <Icon height={19} icon="ion:share-outline" />
+                    </span>
+                  </p>
+                  <p className="mt-6 text-sm">
+                    2. Select{" "}
+                    <span className="bg-white px-2 py-1 rounded">
+                      Add to home screen
+                    </span>
                   </p>
                 </div>
               </div>
-              <div className="h-[1px] w-auto bg-neutral-800/20 mt-6"></div>
-              <div className="p-5">
-                <p className="text-sm leading-6">
-                  To receive push notifications in your iOS device you need to
-                  add this to home screen. Follow the steps below.
-                </p>
-
-                <p className="mt-10 text-sm flex items-center">
-                  1. Tap on{" "}
-                  <span className="bg-white p-2 text-blue-500 ml-1 rounded-md inline-flex items-center justify-center">
-                    <Icon height={19} icon="ion:share-outline" />
-                  </span>
-                </p>
-                <p className="mt-6 text-sm">
-                  2. Select{" "}
-                  <span className="bg-white px-2 py-1 rounded">
-                    Add to home screen
-                  </span>
-                </p>
-              </div>
             </div>
-          </div>
-        </div>
-      )}
+          </motion.div>
+        )}
+      </AnimatePresence>
 
       <FAB />
     </>
