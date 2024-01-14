43a9182b5b0305d3fd2c8ad622dc3a4eb7553449
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-01-14 19:42:44 +0530

    screen zoom

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,10 @@ export default function Document() {
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon-256x256.png"></link>
         <link rel="icon" href="/icon-192x192.png" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+        />
       </Head>
       <body>
         <Main />
