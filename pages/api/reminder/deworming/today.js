7ee1358b925b84f88b92889a98ba18b3ea552db8
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-01-18 01:23:40 +0530

    Refactor deworming notification logic

--- pages/api/reminder/deworming/today.js
+++ pages/api/reminder/deworming/today.js
@@ -9,20 +9,22 @@ export default async function handler(req, res) {
     "You have scheduled dewormings for your pets today. Please check the app for more details.";
   const emails = await getDueDewormingsToday();
 
-  for (let i = 0; i < emails.length; i++) {
-    let userTokens = await getFCMTokens(emails[i]);
-    tokens = tokens.concat(userTokens);
-  }
-
-  let sentResponse = await sendBulkNotification(
-    tokens,
-    messageTitle,
-    messageBody
-  );
-
-  if (sentResponse) {
-    res.status(200).json({ message: "Notification sent" });
+  if (emails.length == 0) {
+    res.status(200).json({ message: "No notifications sent" });
   } else {
-    res.status(200).json({ message: "Something went wrong" });
+    for (let i = 0; i < emails.length; i++) {
+      let userTokens = await getFCMTokens(emails[i]);
+      tokens = tokens.concat(userTokens);
+    }
+    let sentResponse = await sendBulkNotification(
+      tokens,
+      messageTitle,
+      messageBody
+    );
+    if (sentResponse) {
+      res.status(200).json({ message: "Notification sent" });
+    } else {
+      res.status(200).json({ message: "Something went wrong" });
+    }
   }
 }
