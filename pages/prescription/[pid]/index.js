e5a95841b8056bb2e420604598a17de755ce5ea6
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-01-19 22:15:36 +0530

    prescription exists

--- pages/prescription/[pid]/index.js
+++ pages/prescription/[pid]/index.js
@@ -106,6 +106,8 @@ export default function Prescription() {
           } else {
             toast.error(petRequest.data.message);
           }
+        } else {
+          setPageLoaded(true);
         }
       })();
     }
@@ -115,156 +117,166 @@ export default function Prescription() {
     <>
       {pageLoaded ? (
         <>
-          {isAllowed ? (
-            <div>
-              <p className="text-center mt-20 text-sm lg:mt-16 text-neutral-500">
-                Prescription uploaded for
-              </p>
-
-              <div className="w-fit mx-auto relative">
-                <img
-                  src={record.image}
-                  className="h-32 w-32 rounded-full object-cover mx-auto mt-10"
-                  alt=""
-                />
-                <button
-                  onClick={() => router.push(`/pets/${pet?.id}`)}
-                  className="absolute bottom-4 -right-1 h-8 w-8 bg-white hover:bg-neutral-200 rounded-full flex items-center justify-center"
-                >
-                  <Icon icon="material-symbols:pets" />
-                </button>
-              </div>
-
-              <h1 className="text-2xl lg:text-3xl font-semibold text-center mt-4">
-                {record.name}
-              </h1>
-              <div className="flex items-center justify-center mt-3">
-                <button className="text-sm text-neutral-500">
-                  Something&apos;s wrong with this record?{" "}
-                  <span className="text-blue-600">Report</span>
-                </button>
-              </div>
-              <div className="max-w-3xl mx-3 lg:mx-auto pb-16 mt-10 lg:mt-16 ">
-                <div className="grid grid-cols-1 lg:grid-cols-2 gap-2 lg:gap-3">
-                  <div className="border h-16 rounded-md relative flex items-center px-4">
-                    <span className="absolute top-0 text-neutral-400 -translate-y-1/2 left-2 text-xs px-2 bg-white">
-                      Reason for visit
-                    </span>
-                    <p>{record?.reasonOfVisit}</p>
-                  </div>
-                  <div className="border h-16 rounded-md relative flex items-center px-4">
-                    <span className="absolute top-0 text-neutral-400 -translate-y-1/2 left-2 text-xs px-2 bg-white">
-                      Date of visit
-                    </span>
-                    <p>
-                      {new Date(record?.dateOfVisit).toDateString({
-                        year: "numeric",
-                        month: "long",
-                        day: "numeric",
-                      })}
-                    </p>
-                  </div>
-                  <div className="border h-16 rounded-md relative flex items-center px-4">
-                    <span className="absolute top-0 text-neutral-400 -translate-y-1/2 left-2 text-xs px-2 bg-white">
-                      Doctor
-                    </span>
-                    <p>{Capitalize(record?.doctorName)}</p>
-                  </div>
-                  <div className="border h-16 rounded-md relative flex items-center px-4">
-                    <span className="absolute top-0 text-neutral-400 -translate-y-1/2 left-2 text-xs px-2 bg-white">
-                      Species
-                    </span>
-                    <p>{Capitalize(pet?.species)}</p>
-                  </div>
-                  <div className="border h-16 rounded-md relative flex items-center px-4">
-                    <span className="absolute top-0 text-neutral-400 -translate-y-1/2 left-2 text-xs px-2 bg-white">
-                      Breed
-                    </span>
-                    <p>{Capitalize(pet?.breed)}</p>
-                  </div>
-                  <div className="border h-16 rounded-md relative flex items-center px-4">
-                    <span className="absolute top-0 text-neutral-400 -translate-y-1/2 left-2 text-xs px-2 bg-white">
-                      Age on visit
-                    </span>
-                    <p>{calculateAge(pet?.dateOfBirth, record.dateOfVisit)}</p>
-                  </div>
-                  <div className="border h-16 rounded-md relative flex items-center px-4">
-                    <span className="absolute top-0 text-neutral-400 -translate-y-1/2 left-2 text-xs px-2 bg-white">
-                      Colour
-                    </span>
-                    <p>{Capitalize(pet?.color)}</p>
-                  </div>
-                  <div className="border h-16 rounded-md relative flex items-center px-4">
-                    <span className="absolute top-0 text-neutral-400 -translate-y-1/2 left-2 text-xs px-2 bg-white">
-                      Sex
-                    </span>
-                    <p>{Capitalize(pet?.sex)}</p>
-                  </div>
-                  <div className="border h-16 rounded-md relative flex items-center px-4">
-                    <span className="absolute top-0 text-neutral-400 -translate-y-1/2 left-2 text-xs px-2 bg-white">
-                      Temperature
-                    </span>
-                    <p>{Capitalize(record.temperature)} ℉</p>
-                  </div>
-                  <div className="border h-16 rounded-md relative flex items-center px-4">
-                    <span className="absolute top-0 text-neutral-400 -translate-y-1/2 left-2 text-xs px-2 bg-white">
-                      Body weight
-                    </span>
-                    <p>{Capitalize(record?.bodyWeight)} Kg</p>
-                  </div>
-                  <div className="border h-24 rounded-md relative flex py-4 px-4 col-span-2">
-                    <span className="absolute top-0 text-neutral-400 -translate-y-1/2 left-2 text-xs px-2 bg-white">
-                      Notes
-                    </span>
-                    <p>{Capitalize(record?.notes)}</p>
-                  </div>
-                </div>
-                <div className=" my-16"></div>
+          {record ? (
+            <>
+              {isAllowed ? (
                 <div>
-                  <h1 className="text-xl font-semibold text-neutral-700">
-                    Attached documents
-                  </h1>
-                  <div className="space-y-3 mt-4">
-                    {record?.files?.map((file, index) => {
-                      return (
-                        <a
-                          rel="noopener noreferrer"
-                          className="text-sm text-blue-600 flex items-center hover:underline"
-                          href={file}
-                        >
-                          <Icon icon="uiw:paper-clip" />{" "}
-                          <span className="ml-2">View document</span>
-                        </a>
-                      );
-                    })}
-                  </div>
-                  <div className="space-y-3 mt-4"></div>
-                </div>
-                {isParent && (
-                  <div className="p-5 rounded-md mt-24 border">
-                    <h1>Danger zone</h1>
-                    <p className="text-xs text-neutral-500 mt-2">
-                      This action is irreversible & will delete this record
-                      completely.
-                    </p>
-                    <Button
-                      onPress={() =>
-                        Router.push(`/prescription/${record?.id}/delete`)
-                      }
-                      radius="full"
-                      className="px-6 py-2 bg-red-600 text-sm text-white mt-5"
+                  <p className="text-center mt-20 text-sm lg:mt-16 text-neutral-500">
+                    Prescription uploaded for
+                  </p>
+
+                  <div className="w-fit mx-auto relative">
+                    <img
+                      src={record.image}
+                      className="h-32 w-32 rounded-full object-cover mx-auto mt-10"
+                      alt=""
+                    />
+                    <button
+                      onClick={() => router.push(`/pets/${pet?.id}`)}
+                      className="absolute bottom-4 -right-1 h-8 w-8 bg-white hover:bg-neutral-200 rounded-full flex items-center justify-center"
                     >
-                      Delete
-                    </Button>
+                      <Icon icon="material-symbols:pets" />
+                    </button>
                   </div>
-                )}
-              </div>
-            </div>
+
+                  <h1 className="text-2xl lg:text-3xl font-semibold text-center mt-4">
+                    {record.name}
+                  </h1>
+                  <div className="flex items-center justify-center mt-3">
+                    <button className="text-sm text-neutral-500">
+                      Something&apos;s wrong with this record?{" "}
+                      <span className="text-blue-600">Report</span>
+                    </button>
+                  </div>
+                  <div className="max-w-3xl mx-3 lg:mx-auto pb-16 mt-10 lg:mt-16 ">
+                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-2 lg:gap-3">
+                      <div className="border h-16 rounded-md relative flex items-center px-4">
+                        <span className="absolute top-0 text-neutral-400 -translate-y-1/2 left-2 text-xs px-2 bg-white">
+                          Reason for visit
+                        </span>
+                        <p>{record?.reasonOfVisit}</p>
+                      </div>
+                      <div className="border h-16 rounded-md relative flex items-center px-4">
+                        <span className="absolute top-0 text-neutral-400 -translate-y-1/2 left-2 text-xs px-2 bg-white">
+                          Date of visit
+                        </span>
+                        <p>
+                          {new Date(record?.dateOfVisit).toDateString({
+                            year: "numeric",
+                            month: "long",
+                            day: "numeric",
+                          })}
+                        </p>
+                      </div>
+                      <div className="border h-16 rounded-md relative flex items-center px-4">
+                        <span className="absolute top-0 text-neutral-400 -translate-y-1/2 left-2 text-xs px-2 bg-white">
+                          Doctor
+                        </span>
+                        <p>{Capitalize(record?.doctorName)}</p>
+                      </div>
+                      <div className="border h-16 rounded-md relative flex items-center px-4">
+                        <span className="absolute top-0 text-neutral-400 -translate-y-1/2 left-2 text-xs px-2 bg-white">
+                          Species
+                        </span>
+                        <p>{Capitalize(pet?.species)}</p>
+                      </div>
+                      <div className="border h-16 rounded-md relative flex items-center px-4">
+                        <span className="absolute top-0 text-neutral-400 -translate-y-1/2 left-2 text-xs px-2 bg-white">
+                          Breed
+                        </span>
+                        <p>{Capitalize(pet?.breed)}</p>
+                      </div>
+                      <div className="border h-16 rounded-md relative flex items-center px-4">
+                        <span className="absolute top-0 text-neutral-400 -translate-y-1/2 left-2 text-xs px-2 bg-white">
+                          Age on visit
+                        </span>
+                        <p>
+                          {calculateAge(pet?.dateOfBirth, record.dateOfVisit)}
+                        </p>
+                      </div>
+                      <div className="border h-16 rounded-md relative flex items-center px-4">
+                        <span className="absolute top-0 text-neutral-400 -translate-y-1/2 left-2 text-xs px-2 bg-white">
+                          Colour
+                        </span>
+                        <p>{Capitalize(pet?.color)}</p>
+                      </div>
+                      <div className="border h-16 rounded-md relative flex items-center px-4">
+                        <span className="absolute top-0 text-neutral-400 -translate-y-1/2 left-2 text-xs px-2 bg-white">
+                          Sex
+                        </span>
+                        <p>{Capitalize(pet?.sex)}</p>
+                      </div>
+                      <div className="border h-16 rounded-md relative flex items-center px-4">
+                        <span className="absolute top-0 text-neutral-400 -translate-y-1/2 left-2 text-xs px-2 bg-white">
+                          Temperature
+                        </span>
+                        <p>{Capitalize(record.temperature)} ℉</p>
+                      </div>
+                      <div className="border h-16 rounded-md relative flex items-center px-4">
+                        <span className="absolute top-0 text-neutral-400 -translate-y-1/2 left-2 text-xs px-2 bg-white">
+                          Body weight
+                        </span>
+                        <p>{Capitalize(record?.bodyWeight)} Kg</p>
+                      </div>
+                      <div className="border h-24 rounded-md relative flex py-4 px-4 col-span-2">
+                        <span className="absolute top-0 text-neutral-400 -translate-y-1/2 left-2 text-xs px-2 bg-white">
+                          Notes
+                        </span>
+                        <p>{Capitalize(record?.notes)}</p>
+                      </div>
+                    </div>
+                    <div className=" my-16"></div>
+                    <div>
+                      <h1 className="text-xl font-semibold text-neutral-700">
+                        Attached documents
+                      </h1>
+                      <div className="space-y-3 mt-4">
+                        {record?.files?.map((file, index) => {
+                          return (
+                            <a
+                              rel="noopener noreferrer"
+                              className="text-sm text-blue-600 flex items-center hover:underline"
+                              href={file}
+                            >
+                              <Icon icon="uiw:paper-clip" />{" "}
+                              <span className="ml-2">View document</span>
+                            </a>
+                          );
+                        })}
+                      </div>
+                      <div className="space-y-3 mt-4"></div>
+                    </div>
+                    {isParent && (
+                      <div className="p-5 rounded-md mt-24 border">
+                        <h1>Danger zone</h1>
+                        <p className="text-xs text-neutral-500 mt-2">
+                          This action is irreversible & will delete this record
+                          completely.
+                        </p>
+                        <Button
+                          onPress={() =>
+                            Router.push(`/prescription/${record?.id}/delete`)
+                          }
+                          radius="full"
+                          className="px-6 py-2 bg-red-600 text-sm text-white mt-5"
+                        >
+                          Delete
+                        </Button>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              ) : (
+                <p className="text-center text-sm mx-5 leading-6 mt-16">
+                  This record is private and can only be viewed by the parent.
+                  Ask parent to make their pet&apos;s profile public.
+                </p>
+              )}
+            </>
           ) : (
-            <p className="text-center text-sm mx-5 leading-6 mt-16">
-              This record is private and can only be viewed by the parent. Ask
-              parent to make their pet&apos;s profile public.
-            </p>
+            <div>
+              <p>This record does not exist or has been deleted.</p>
+            </div>
           )}
         </>
       ) : (
