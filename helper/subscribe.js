6c085c4b608885dae9072ebd786d6ce7917ca798
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-01-20 20:02:23 +0530

    Refactor subscribe function to remove unnecessary code and improve readability

--- helper/subscribe.js
+++ helper/subscribe.js
@@ -2,62 +2,53 @@ import firebaseApp from "@/firebase/app";
 import axios from "axios";
 import { getMessaging, getToken } from "firebase/messaging";
 import { getSession } from "next-auth/react";
-import toast from "react-hot-toast";
 
-export const subscribe = async (showToast) => {
+export const subscribe = async () => {
   let session = await getSession();
   let messaging = getMessaging(firebaseApp);
   try {
     if (typeof window !== "undefined" && "serviceWorker" in navigator) {
-      if (Notification.permission == "granted") {
-        const token = await getToken(messaging, {
-          vapidKey:
-            "BMz9a6zyrHPgp5jBxXv_QjIhcJaunKrX2zinqT1ThGEeckAsbD2J0BdQYpd-SHSf8beu9ngbsUfI3iTVoklKLOo",
-        });
-        if (token) {
-          console.log(token);
-          try {
-            await axios.post(
-              "/api/fcm/update",
-              {
-                email: session.user.email,
-                token: token,
-              },
-              {
-                headers: {
-                  "Content-Type": "application/json",
+      if ("Notification" in window) {
+        if (Notification.permission == "granted") {
+          const token = await getToken(messaging, {
+            vapidKey:
+              "BMz9a6zyrHPgp5jBxXv_QjIhcJaunKrX2zinqT1ThGEeckAsbD2J0BdQYpd-SHSf8beu9ngbsUfI3iTVoklKLOo",
+          });
+          if (token) {
+            try {
+              await axios.post(
+                "/api/fcm/update",
+                {
+                  email: session.user.email,
+                  token: token,
                 },
-              }
-            );
-            return token;
-          } catch (error) {
-            showToast &&
-              toast.error("Error while subscribing to push notifications");
-            console.log(error);
+                {
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                }
+              );
+              return token;
+            } catch (error) {
+              return null;
+            }
+          } else {
+            await subscribe();
             return null;
           }
-        } else {
-          showToast &&
-            toast.error("Error while subscribing to push notifications");
-
-          await subscribe();
-          return null;
         }
       }
     }
   } catch (error) {
-    console.log("Service worker registration failed, error:", error);
     await navigator.serviceWorker
       .register("/firebase-messaging-sw.js")
       .then(async function (reg) {
         if (reg.installing) {
         } else if (reg.waiting) {
         } else if (reg.active) {
-          toast.dismiss();
           await subscribe(true);
         }
       });
-    toast.dismiss();
     await subscribe(true);
   }
 };
