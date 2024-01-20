f5e536e637bc1c4b10d8c940ca78c05e7814a591
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-01-20 19:06:15 +0530

    messaging

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,3 @@
-import { isSupported } from "firebase/messaging";
-
 importScripts("https://www.gstatic.com/firebasejs/8.8.0/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.8.0/firebase-messaging.js");
 
