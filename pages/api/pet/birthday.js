e725c3251c3512a5302f4bf0186bf515d4afb562
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-01-21 23:37:16 +0530

    Commented out code related to sending birthday emails

--- pages/api/pet/birthday.js
+++ pages/api/pet/birthday.js
@@ -1,27 +1,27 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { sendMail } from "@/helper/sendMail";
-import { getBirthdaysToday } from "@/prisma/pet";
-import { TodayBirthday } from "@/templates/Birthday";
+// import { sendMail } from "@/helper/sendMail";
+// import { getBirthdaysToday } from "@/prisma/pet";
+// import { TodayBirthday } from "@/templates/Birthday";
 
-export default async function handler(req, res) {
-  let { pets, success, message } = await getBirthdaysToday();
+// export default async function handler(req, res) {
+//   let { pets, success, message } = await getBirthdaysToday();
 
-  for (let i = 0; i < pets.length; i++) {
-    let pet = pets[i];
-    try {
-      let { message } = await sendMail(
-        process.env.ZOHO_MAIL,
-        process.env.ZOHO_PASS,
-        pet.email,
-        `Happy Birthday ${pet.name} 🎂`,
-        TodayBirthday(pet.name)
-      );
-      console.log(message);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+//   for (let i = 0; i < pets.length; i++) {
+//     let pet = pets[i];
+//     try {
+//       //   let { message } = await sendMail(
+//       //     process.env.ZOHO_MAIL,
+//       //     process.env.ZOHO_PASS,
+//       //     pet.email,
+//       //     `Happy Birthday ${pet.name} 🎂`,
+//       //     TodayBirthday(pet.name)
+//       //   );
+//       console.log(message);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }
 
-  res.status(200).json({ success, message });
-}
+//   res.status(200).json({ success, message });
+// }
