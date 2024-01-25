9cd9ef7d3b302b7df399c102121a060997e741a7
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-01-26 00:33:14 +0530

    Commented out unused code and returned null session

--- pages/api/session.js
+++ pages/api/session.js
@@ -3,6 +3,6 @@ import { getServerSession } from "next-auth/next";
 import { getDueDewormingsTomorrow } from "@/prisma/deworming";
 
 export default async function handler(req, res) {
-  const session = await getDueDewormingsTomorrow();
-  res.status(200).json(session);
+  //const session = await getDueDewormingsTomorrow();
+  res.status(200).json({ session: null });
 }
