fc169a7799bac46e2f8ab45944b435982483638b
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-01-26 12:38:49 +0530

    Commented out Republic Day notification code

--- pages/api/campaigns/republicday.js
+++ pages/api/campaigns/republicday.js
@@ -7,19 +7,19 @@ import { GetAllUsers } from "@/prisma/user";
 import { Republicday } from "@/templates/Republicday";
 
 export default async function handler(req, res) {
-  let title = "Happy Republic Day 🇮🇳";
-  let body =
-    "We wish you a very happy republic day. We are proud to be an Indian.";
-  const fcms = await getFCMTokens();
-  let emails = await GetAllUsers();
-  //await sendBulkNotification(fcms, title, body);
-  await sendBulkMail(
-    process.env.ZOHO_MAIL,
-    process.env.ZOHO_PASS,
-    ["devpriyangsu@gmail.com"],
-    "Happy Republic Day 🇮🇳",
-    Republicday()
-  );
+  //   let title = "Happy Republic Day 🇮🇳";
+  //   let body =
+  //     "We wish you a very happy republic day. We are proud to be an Indian.";
+  //   const fcms = await getFCMTokens();
+  //   let emails = await GetAllUsers();
+  //   //await sendBulkNotification(fcms, title, body);
+  //   await sendBulkMail(
+  //     process.env.ZOHO_MAIL,
+  //     process.env.ZOHO_PASS,
+  //     ["devpriyangsu@gmail.com"],
+  //     "Happy Republic Day 🇮🇳",
+  //     Republicday()
+  //   );
   res.status(200).json({
     message: "Notification sent",
   });
