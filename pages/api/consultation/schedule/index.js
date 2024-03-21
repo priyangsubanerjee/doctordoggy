66a376e0fbd3f5971f03a0d2e73a4865e82234a1
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-03-21 14:32:17 +0530

    Change handler function to default export in consultation schedule API

--- pages/api/consultation/schedule/index.js
+++ pages/api/consultation/schedule/index.js
@@ -1,4 +1,4 @@
-export async function handler(req, res) {
+export default async function handler(req, res) {
   res.json({
     message: "Consultation server is up",
     time: new Date().toISOString(),
