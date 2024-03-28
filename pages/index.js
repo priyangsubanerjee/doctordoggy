3c9aae8c940481068d8f95b7ec0f43f4ca75fd3f
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-03-28 21:50:54 +0530

    Add meta description for pet care app

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,10 @@ export default function Home() {
     <main>
       <Head>
         <title>Doctor Doggy | Home</title>
+        <meta
+          name="description"
+          content="Dr. Doggy, your all-in-one pet care app, keeps your furry friend happy and healthy by offering secure digital pet records, convenient online vet consultations, and booking for pet care services like pet sitting and dog walking. Plus, you'll receive timely notifications to ensure you never miss a vaccination or checkup again!"
+        />
       </Head>
       <HeroSection />
       <Services />
